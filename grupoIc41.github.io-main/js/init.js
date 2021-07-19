2c7dc3efc83da87b437c0973daa2e9b38b6e6f03
AngelLedesma123 <87627845+AngelLedesma123@users.noreply.github.com>
2021-07-18 22:26:02 -0500

    Update init.js

--- grupoIc41.github.io-main/js/init.js
+++ grupoIc41.github.io-main/js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyAXHRomVUOwtDN-Q85rG6oTgknItfFU-d8",
-    authDomain: "chat41h.firebaseapp.com",
-    projectId: "chat41h",
-    storageBucket: "chat41h.appspot.com",
-    messagingSenderId: "27366034098",
-    appId: "1:27366034098:web:0c5f90072b9f7cedf017ab",
-    measurementId: "G-HGKL9C6LV0"
+     apiKey: "AIzaSyCs-vyZyxtJcGu0wMnSzlMDoyfQMXHMjaA",
+    authDomain: "chat41-841b0.firebaseapp.com",
+    projectId: "chat41-841b0",
+    storageBucket: "chat41-841b0.appspot.com",
+    messagingSenderId: "1072446386423",
+    appId: "1:1072446386423:web:9aea483a45da8bc410a3f2",
+    measurementId: "G-RJVVFWJJR5"
     });
